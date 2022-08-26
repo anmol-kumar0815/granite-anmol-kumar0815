abe31168c9c677798c27230efb4cb84a960e2af7
Anmol Kumar <anmolshrivastav.08@gmail.com>
2022-08-27 02:20:26 +0530

    Added tasks controller and index action

--- babel.config.js
+++ babel.config.js
@@ -37,7 +37,6 @@ module.exports = function(api) {
       ]
     ].filter(Boolean),
     plugins: [
-      "js-logger",
       'babel-plugin-macros',
       '@babel/plugin-syntax-dynamic-import',
       isTestEnv && 'babel-plugin-dynamic-import-node',
