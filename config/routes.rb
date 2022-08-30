778afb0eb48ffc597b53083a5fcb1c4e0f3a2b55
Anmol Kumar <anmolshrivastav.08@gmail.com>
2022-08-31 01:32:27 +0530

    Added slug to task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tasks, only: :index
+  # resources :tasks, only: :index
+  # resources: tasks, param: :identifier_name
+  # @task = Task.find_by(identifier_name: params[:identifier_name])
+  resources :tasks, only: :index, param: :slug
 
   root "home#index"
   get "*path", to: "home#index", via: :all
