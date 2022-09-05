dddb24ff234b9908f2a11cd42e65ec432deac455
Anmol Kumar <anmolshrivastav.08@gmail.com>
2022-09-05 11:11:43 +0530

    Added pundit task policy

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ group :development, :test do
   # For auto-generating demo data
   gem "faker"
 end
+
+gem "pundit"
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,39 @@
-# frozen_string_literal: true
-
 class TasksController < ApplicationController
   before_action :load_task!, only: %i[show update destroy]
 
+  after_action :verify_authorized, except: :index
+  after_action :verify_policy_scoped, only: :index
+
+  rescue_from Pundit::NotAuthorizedError, with: :handle_authorization_error
+
   def index
-    tasks = Task.all.as_json(include: { assigned_user: { only: %i[name id] } })
-    respond_with_json({ tasks: tasks })
+    tasks = policy_scope(Task)
+    tasks_with_assigned_user = tasks.as_json(include: { assigned_user: { only: %i[name id] } })
+    respond_with_json(tasks_with_assigned_user)
   end
 
-  def show
-    render
-  end
-
-  def update
-    @task.update!(task_params)
-    respond_with_success(t("successfully_updated"))
-  end
-
-  def destroy
-    @task.destroy!
-    respond_with_json
-  end
 
   def create
     task = current_user.created_tasks.new(task_params)
+    authorize task
     task.save!
     respond_with_success(t("successfully_created", entity: "Task"))
+  end
+
+  def show
+    authorize @task
+  end
+
+  def update
+    authorize @task
+    @task.update!(task_params)
+    respond_with_success(t("successfully_updated", entity: "Task")
+  end
+
+  def destroy
+    authorize @task
+    @task.destroy!
+    respond_with_json
   end
 
   private
@@ -36,5 +44,9 @@ class TasksController < ApplicationController
 
     def load_task!
       @task = Task.find_by!(slug: params[:slug])
+    end
+
+    def handle_authorization_error
+      respond_with_error("Access denied. You are not authorized to perform this action.", :forbidden)
     end
 end
